040f960d4b77808c9f1c647a1db6344740864a8c
Stephen Doyle <stephen.doyle@intel.com>
2024-09-25 13:22:57 +0100

    Change P2416 to a trie based solution

--- solutions/p2416_sum_of_prefix_scores_of_strings.py
+++ solutions/p2416_sum_of_prefix_scores_of_strings.py
@@ -5,8 +5,49 @@
 from typing import List
 
 
+class trie_node:
+    def __init__(self):
+        self.next = [None] * 26
+        self.cnt = 0
+
+
 class Solution:
-    def sumPrefixScores(self, words: List[str]) -> List[int]:
+    def __init__(self):
+        # Initialize the root node of the trie.
+        self.root = trie_node()
+
+    # Insert function for the word.
+    def insert(self, word):
+        node = self.root
+        for c in word:
+            idx = ord(c) - ord("a")
+            # If new prefix, create a new trie node.
+            if node.next[idx] is None:
+                node.next[idx] = trie_node()
+            # Increment the count of the current prefix.
+            node.next[idx].cnt += 1
+            node = node.next[idx]
+
+    # Calculate the prefix count using this function.
+    def count(self, s):
+        node = self.root
+        ans = 0
+        # The ans would store the total sum of counts.
+        for c in s:
+            idx = ord(c) - ord("a")
+            ans += node.next[idx].cnt
+            node = node.next[idx]
+        return ans
+
+    def sumPrefixScores(self, words):
+        # Insert words in trie.
+        for word in words:
+            self.insert(word)
+        # Calculate the prefix count for each word.
+        scores = [self.count(word) for word in words]
+        return scores
+
+    def sumPrefixScoresSlow(self, words: List[str]) -> List[int]:
         scores = []
         dp = {}
         for word in words:
@@ -41,6 +82,14 @@ class TestSumPrefixSocres:
     def test_example2(self):
         words = ["abcd"]
         assert Solution().sumPrefixScores(words) == [4]
+
+    def test_example1_slow(self):
+        words = ["abc", "ab", "bc", "b"]
+        assert Solution().sumPrefixScoresSlow(words) == [5, 4, 3, 2]
+
+    def test_example2_slow(self):
+        words = ["abcd"]
+        assert Solution().sumPrefixScoresSlow(words) == [4]
 
     def test_prefixes(self):
         word = "abc"
