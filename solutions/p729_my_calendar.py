f27424e4b9055d1df537aa4d43aa0f3c6dddbdb2
Stephen Doyle <stephendoyle75@gmail.com>
2024-09-26 12:22:42 +0100

    Optimize P729 solution to use binary serach and sorted calendar

--- solutions/p729_my_calendar.py
+++ solutions/p729_my_calendar.py
@@ -3,6 +3,37 @@ class MyCalendar:
         self.calendar = []
 
     def book(self, start: int, end: int) -> bool:
+        """Optimized solution"""
+        # Binary search to find where the new booking should be inserted
+        idx = self.search(start)
+
+        if idx > 0 and start < self.calendar[idx - 1][1]:
+            # This booking starts before the previous booking ends
+            return False
+
+        if idx < len(self.calendar) and end > self.calendar[idx][0]:
+            # This booking ends after the next booking starts
+            return False
+
+        # Insert the new booking
+        # Keep calendar sorted by start time
+        self.calendar.insert(idx, (start, end))
+        return True
+
+    def search(self, start) -> int:
+        """Binary search to find where the new booking should be inserted"""
+        left, right = 0, len(self.calendar)
+        while left < right:
+            mid = (left + right) // 2
+            if self.calendar[mid][0] >= start:
+                right = mid
+            else:
+                left = mid + 1
+        return left
+
+    # Time: O(n^2) for n bookings, Space: O(n)
+    def book_slow(self, start: int, end: int) -> bool:
+        """Brute force solution"""
         for s, e in self.calendar:
             if start < e and end > s:
                 return False
@@ -13,6 +44,12 @@ class MyCalendar:
 class TestMyCalendar:
     def test_example1(self):
         calendar = MyCalendar()
-        assert calendar.book(10, 20) == True
-        assert calendar.book(15, 25) == False
-        assert calendar.book(20, 30) == True
+        assert calendar.book(10, 20)
+        assert not calendar.book(15, 25)
+        assert calendar.book(20, 30)
+
+    def test_example1_slow(self):
+        calendar = MyCalendar()
+        assert calendar.book_slow(10, 20)
+        assert not calendar.book_slow(15, 25)
+        assert calendar.book_slow(20, 30)
