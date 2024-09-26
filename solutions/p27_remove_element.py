3d6829cb9848bd7d1fe640ec5108f43f9e0c37c6
Stephen Doyle <stephendoyle75@gmail.com>
2024-09-26 13:21:28 +0100

    Optimizing P27 solution

--- solutions/p27_remove_element.py
+++ solutions/p27_remove_element.py
@@ -10,8 +10,21 @@ class Solution:
         k = 0  # index of the next element to be replaced
         for i in range(len(nums)):
             if nums[i] != val:
+                # Swaps the elements that are not equal to the target value
                 nums[k] = nums[i]
                 k += 1
+        return k
+
+    def removeRareElement(self, nums: List[int], val: int) -> int:
+        i = 0
+        k = len(nums)
+        while i < k:
+            if nums[i] == val:
+                # Only swap the element when it is equal to the target value
+                nums[i] = nums[k - 1]
+                k -= 1
+            else:
+                i += 1
         return k
 
 
@@ -23,3 +36,11 @@ class TestRemoveElement:
     def test_example2(self):
         nums = [0, 1, 2, 2, 3, 0, 4, 2]
         assert Solution().removeElement(nums, 2) == 5
+
+    def test_rare_example1(self):
+        nums = [3, 2, 2, 3]
+        assert Solution().removeRareElement(nums, 3) == 2
+
+    def test_rare_example2(self):
+        nums = [0, 1, 2, 2, 3, 0, 4, 2]
+        assert Solution().removeRareElement(nums, 2) == 5
