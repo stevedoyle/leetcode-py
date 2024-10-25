2722bd9ff2da9c87ce5555176d7294c2b2d994a4
Stephen Doyle <stephendoyle75@gmail.com>
2024-10-25 11:19:54 +0100

    Add P290

---
+++ solutions/p290_word_pattern.py
@@ -0,0 +1,51 @@
+# Title: 290. Word Pattern
+# URL: https://leetcode.com/problems/word-pattern/
+# Difficulty: Easy
+
+
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        words = s.split()
+        if len(words) != len(pattern):
+            return False
+        map = {}
+        seen = set()
+
+        for letter, word in zip(pattern, words):
+            if letter in map:
+                if map[letter] != word:
+                    return False
+            elif word in seen:
+                return False
+            else:
+                map[letter] = word
+                seen.add(word)
+
+        return True
+
+
+class TestWordPattern:
+    def test_example1(self):
+        pattern = "abba"
+        s = "dog cat cat dog"
+        assert Solution().wordPattern(pattern, s)
+
+    def test_example2(self):
+        pattern = "abba"
+        s = "dog cat cat fish"
+        assert not Solution().wordPattern(pattern, s)
+
+    def test_example3(self):
+        pattern = "aaaa"
+        s = "dog cat cat dog"
+        assert not Solution().wordPattern(pattern, s)
+
+    def test_example4(self):
+        pattern = "abba"
+        s = "dog dog dog dog"
+        assert not Solution().wordPattern(pattern, s)
+
+    def test_example5(self):
+        pattern = "abc"
+        s = "dog cat dog"
+        assert not Solution().wordPattern(pattern, s)
