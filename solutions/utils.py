1aa56eeb05ac3ece06d143db8903ac05d07cd479
Stephen Doyle <stephendoyle75@gmail.com>
2025-05-23 17:52:07 +0100

    Fix warnings in utils.py

--- solutions/utils.py
+++ solutions/utils.py
@@ -74,22 +74,22 @@ class Node:
 
 class TestNode:
     def test_from_list(self):
-        input = [[2, 4], [1, 3], [2, 4], [1, 3]]
+        data = [[2, 4], [1, 3], [2, 4], [1, 3]]
         expected = [[2, 4], [1, 3], [2, 4], [1, 3]]
-        result = Node.from_list(input)
+        result = Node.from_list(data)
         result = result.to_list() if result else None
         assert result == expected
 
     def test_to_list(self):
-        input = Node(1, [Node(2), Node(3)])
+        data = Node(1, [Node(2), Node(3)])
         expected = [[2, 3], [], []]
-        result = input.to_list()
+        result = data.to_list()
         assert result == expected
 
     def test_to_list_single_node(self):
-        input = Node(1)
+        data = Node(1)
         expected = [[]]
-        result = input.to_list()
+        result = data.to_list()
         assert result == expected
 
 
